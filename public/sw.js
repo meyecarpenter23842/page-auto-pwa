be512a7ac0438cfd43e6e10b292d01864915b1d3
meyecarpenter23842 <meyecarpenter23842@hotmail.com>
2026-09-08 10:19:08 +0700

    chore: add basic service worker

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+const CACHE='page-auto-pwa-v1';
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['/'])));self.skipWaiting()});
+self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).catch(()=>caches.match(event.request).then(response=>response||caches.match('/'))))});
